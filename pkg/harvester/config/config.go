dc7d3eea3e8d259add168ffad17571ec4151353d
Maximiliano Churichi <mchurichi@gmail.com>
2022-07-15 15:59:13 -0300

    Adds basic Harvester config reading
    Signed-off-by: Maximiliano Churichi <mchurichi@gmail.com>

---
+++ pkg/harvester/config/config.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/hashicorp/hcl"
+	"github.com/pkg/errors"
+)
+
+type HarvesterConfig struct {
+	HarvesterConfigSection *HarvesterConfigSection `hcl:"harvester"`
+}
+
+type HarvesterConfigSection struct {
+	SpireSocketPath string `hcl:"spire_socket_path"`
+	ServerAddress   string `hcl:"server_address"`
+	LogLevel        string `hcl:"log_level"`
+}
+
+// New creates a new HarvesterConfig from the given input reader.
+func New(config io.Reader) (*HarvesterConfig, error) {
+
+	if config == nil {
+		return nil, errors.New("configuration is required")
+	}
+
+	configBytes, err := io.ReadAll(config)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read configuration")
+	}
+
+	return new(configBytes)
+}
+
+func new(configBytes []byte) (*HarvesterConfig, error) {
+	var config HarvesterConfig
+
+	if err := hcl.Decode(&config, string(configBytes)); err != nil {
+		return nil, fmt.Errorf("unable to decode configuration: %v", err)
+	}
+
+	if config.HarvesterConfigSection == nil {
+		config.HarvesterConfigSection = &HarvesterConfigSection{}
+	}
+
+	config.setDefaults()
+
+	if err := config.validate(); err != nil {
+		return nil, errors.Wrap(err, "invalid configuration")
+	}
+
+	return &config, nil
+}
+
+func (c *HarvesterConfig) validate() error {
+	if c.HarvesterConfigSection.ServerAddress == "" {
+		return errors.New("harvester.server_address is required")
+	}
+
+	return nil
+}
+
+func (c *HarvesterConfig) setDefaults() {
+	if c.HarvesterConfigSection.LogLevel == "" {
+		c.HarvesterConfigSection.LogLevel = "INFO"
+	}
+
+	if c.HarvesterConfigSection.SpireSocketPath == "" {
+		c.HarvesterConfigSection.SpireSocketPath = "/tmp/spire-server/private/api.sock"
+	}
+}
---
+++ pkg/harvester/config/disk_test.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadFromDisk(t *testing.T) {
+	tests := []struct {
+		name      string
+		path      string
+		expected  *HarvesterConfig
+		errPrefix string
+		err       string
+	}{
+		{
+			name: "ok",
+			path: "testdata/ok.conf",
+			expected: &HarvesterConfig{
+				HarvesterConfigSection: &HarvesterConfigSection{
+					SpireSocketPath: "spire_socket_path",
+					ServerAddress:   "server_address",
+					LogLevel:        "log_level",
+				},
+			},
+		}, {
+			name:     "ok_empty",
+			path:     "testdata/empty.conf",
+			expected: &HarvesterConfig{},
+		},
+		{
+			name:      "cannot_open_file",
+			path:      "invalid_path",
+			expected:  nil,
+			errPrefix: "unable to open configuration file",
+			err:       "open invalid_path: no such file or directory",
+		},
+		{
+			name:     "new_error",
+			path:     "testdata/empty.conf",
+			expected: nil,
+			err:      "unable to parse configuration file: unexpected end of JSON input",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			newFn = func(config io.Reader) (*HarvesterConfig, error) {
+				if tt.err != "" {
+					return nil, fmt.Errorf("%s: %s", tt.errPrefix, tt.err)
+				}
+				return tt.expected, nil
+			}
+
+			got, err := LoadFromDisk(tt.path)
+
+			if tt.err != "" {
+				assert.EqualError(t, err, fmt.Sprintf(tt.errPrefix+": %s", tt.err))
+				assert.Nil(t, got)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, got)
+		})
+	}
+
+}
