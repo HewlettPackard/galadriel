08ccadec3150f0b423861e734ea8dee4844a12c0
Nathalia <nathalia.gomazako@gmail.com>
2022-08-05 11:31:40 -0300

    Apply PR feedbacks
    Signed-off-by: Nathalia <nathalia.gomazako@gmail.com>

--- pkg/common/telemetry/metrics.go
+++ pkg/common/telemetry/metrics.go
@@ -8,7 +8,7 @@ func Count(ctx context.Context, component, entity, action string) {
 	var increment int64
 	increment = 1
 
-	meter := InitiateMeterProvider(ctx)
+	meter := InitializeMeterProvider(ctx)
 
 	label := FormatLabel(component, entity, action)
 	updown_counter, err := meter.SyncInt64().UpDownCounter(
