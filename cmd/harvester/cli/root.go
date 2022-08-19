e892bdf96a2f3c2750a19e58bdaf7d793d6ea2c8
Nathalia <nathalia.gomazako@gmail.com>
2022-08-19 10:54:57 -0300

    Add cobra for harvester cli

--- cmd/harvester/cli/root.go
+++ cmd/harvester/cli/root.go
@@ -4,7 +4,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:  "harvester",
 	Long: "This is Galadriel Harvester CLI",
@@ -12,8 +11,6 @@ var RootCmd = &cobra.Command{
 
 var cmdExecute = RootCmd.Execute
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() int {
 	err := cmdExecute()
 	if err != nil {
