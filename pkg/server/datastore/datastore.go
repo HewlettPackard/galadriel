fce86e67f5223ea13ebfd4cde69cc83e7cfebfe7
Thiago Sabatucci <thisab@gmail.com>
2022-08-31 16:04:08 -0300

    updated datastore.go removing the functions with bridge items. Also updated the Create/Retrieve functions to always return values

--- pkg/server/datastore/datastore.go
+++ pkg/server/datastore/datastore.go
@@ -6,41 +6,36 @@ import (
 	"time"
 
 	management "github.com/HewlettPackard/galadriel/pkg/server/api/management"
-	"github.com/labstack/echo/v4"
 	"github.com/spiffe/go-spiffe/v2/spiffeid"
 	"github.com/spiffe/spire/proto/spire/common"
 )
 
 // DataStore defines the data storage interface.
 type DataStore interface {
-	CreateMember(echo.Context, *management.SpireServer) (*management.SpireServer, error)
+	CreateMember(context.Context, *management.SpireServer) (*management.SpireServer, error)
 	CreateMembership(ctx context.Context, membership *management.FederationGroupMembership, memberID uint, bridgeID uint) (*management.FederationGroupMembership, error)
 	CreateTrustBundle(ctx context.Context, trust *common.Bundle, memberID uint) (*common.Bundle, error)
-	CreateRelationship(ctx context.Context, newrelation *FederationRelationship, sourceID uint, targetID uint) error
-	RetrieveBridgebyID(ctx context.Context, brID uint) (*management.FederationGroup, error)
-	RetrieveAllMembershipsbyBridgeID(ctx context.Context, bridgeID uint) (*[]management.FederationGroupMembership, error)
-	RetrieveAllMembersbyBridgeID(ctx context.Context, bridgeID uint) (*[]management.SpireServer, error)
-	RetrieveAllMembershipsbyMemberID(ctx context.Context, memberID uint) (*[]management.FederationGroupMembership, error)
-	RetrieveAllRelationshipsbyMemberID(ctx context.Context, memberID uint) (*[]FederationRelationship, error)
-	RetrieveAllTrustBundlesbyMemberID(ctx context.Context, memberID uint) (*[]common.Bundle, error)
-	RetrieveMemberbyID(ctx context.Context, memberID uint) (*management.SpireServer, error)
-	RetrieveMembershipbyCreationDate(ctx context.Context, date time.Time) (*management.FederationGroupMembership, error)
-	RetrieveMembershipbyToken(ctx context.Context, token string) (*management.FederationGroupMembership, error)
-	RetrieveRelationshipbySourceandTargetID(ctx context.Context, source uint, target uint) (*FederationRelationship, error)
-	RetrieveTrustbundlebyMemberID(ctx context.Context, memberID string) (*common.Bundle, error)
+	CreateRelationship(ctx context.Context, newrelation *FederationRelationship, sourceID uint, targetID uint) (*FederationRelationship, error)
+	RetrieveAllMembershipsByMemberID(ctx context.Context, memberID uint) (*[]management.FederationGroupMembership, error)
+	RetrieveAllRelationshipsByMemberID(ctx context.Context, memberID uint) (*[]FederationRelationship, error)
+	RetrieveAllTrustBundlesByMemberID(ctx context.Context, memberID uint) (*[]common.Bundle, error)
+	RetrieveMemberByID(ctx context.Context, memberID uint) (*management.SpireServer, error)
+	RetrieveMembershipByCreationDate(ctx context.Context, date time.Time) (*management.FederationGroupMembership, error)
+	RetrieveMembershipByToken(ctx context.Context, token string) (*management.FederationGroupMembership, error)
+	RetrieveRelationshipBySourceandTargetID(ctx context.Context, source uint, target uint) (*FederationRelationship, error)
+	RetrieveTrustbundleByMemberID(ctx context.Context, memberID string) (*common.Bundle, error)
 	UpdateMember(context.Context, management.SpireServer) error
 	UpdateMembership(context.Context, management.FederationGroupMembership) error
 	UpdateTrust(context.Context, common.Bundle) error
-	DeleteOrganizationbyID(ctx context.Context, orgID uint) error
-	DeleteBridgebyID(ctx context.Context, bridgeID uint) error
+	DeleteOrganizationByID(ctx context.Context, orgID uint) error
 	DeleteMemberbyID(ctx context.Context, memberID uint) error
-	DeleteAllMembershipsbyMemberID(ctx context.Context, memberid uint) error
-	DeleteAllMembershipsbyBridgeID(ctx context.Context, bridgeid uint) error
-	DeleteAllRelationshipsbyMemberID(ctx context.Context, memberid uint) error
-	DeleteAllTrustbundlesbyMemberID(ctx context.Context, memberid uint) error
-	DeleteMembershipbyToken(ctx context.Context, name string) error
-	DeleteRelationshipbySourceTargetID(ctx context.Context, source uint, target uint) error
-	DeleteTrustBundlebyMemberID(ctx context.Context, memberID string) error
+	DeleteAllMembershipsByMemberID(ctx context.Context, memberid uint) error
+	DeleteAllMembershipsByBridgeID(ctx context.Context, bridgeid uint) error
+	DeleteAllRelationshipsByMemberID(ctx context.Context, memberid uint) error
+	DeleteAllTrustbundlesByMemberID(ctx context.Context, memberid uint) error
+	DeleteMembershipByToken(ctx context.Context, name string) error
+	DeleteRelationshipBySourceTargetID(ctx context.Context, source uint, target uint) error
+	DeleteTrustBundleByMemberID(ctx context.Context, memberID string) error
 }
 type BundleEndpointType string
 
