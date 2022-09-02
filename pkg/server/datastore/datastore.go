05e5ac53f214459d826b93615f8c5d7c5578a971
Thiago Sabatucci <thisab@gmail.com>
2022-09-02 12:56:55 -0300

    Changed suggestions on datastore.go

--- pkg/server/datastore/datastore.go
+++ pkg/server/datastore/datastore.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	entity "github.com/HewlettPackard/galadriel/pkg/common/entity"
-	"github.com/HewlettPackard/galadriel/pkg/server/api/management"
 	"github.com/spiffe/spire/proto/spire/common"
 )
 
@@ -12,17 +11,17 @@ import (
 type DataStore interface {
 	CreateMember(context.Context, *entity.Member) (*entity.Member, error)
 	CreateMembership(ctx context.Context, membership *entity.Membership) (*entity.Membership, error)
-	CreateTrustBundle(ctx context.Context, trust *common.Bundle, memberID uint) (*common.Bundle, error)
-	CreateRelationship(ctx context.Context, relationship *entity.Relationship, sourceID uint, targetID uint) (*entity.Relationship, error)
-	RetrieveMembershipsByID(ctx context.Context, memberID uint) (*[]management.FederationGroupMembership, error)
+	CreateTrustBundle(ctx context.Context, trustBundle *common.Bundle, memberID uint) (*common.Bundle, error)
+	CreateRelationship(ctx context.Context, relationship *entity.Relationship, sourceMemberId uint, targetMemberId uint) (*entity.Relationship, error)
+	RetrieveMembershipsByID(ctx context.Context, memberID uint) (*[]entity.Membership, error)
 	RetrieveRelationshipsByID(ctx context.Context, memberID uint) (*[]entity.Relationship, error)
 	RetrieveTrustBundlesByID(ctx context.Context, memberID uint) (*[]common.Bundle, error)
 	RetrieveMemberByID(context.Context, entity.Member) (*entity.Member, error)
-	RetrieveMembershipByID(ctx context.Context, membershipID uint) (*management.FederationGroupMembership, error)
+	RetrieveMembershipByID(ctx context.Context, membershipID uint) (*entity.Membership, error)
 	RetrieveRelationshipBySourceandTargetID(ctx context.Context, source uint, target uint) (*entity.Relationship, error)
 	RetrieveTrustbundleByMemberID(ctx context.Context, memberID string) (*common.Bundle, error)
-	UpdateMember(context.Context, management.SpireServer) error
-	UpdateMembership(context.Context, management.FederationGroupMembership) error
+	UpdateMember(context.Context, entity.Member) error
+	UpdateMembership(context.Context, entity.Membership) error
 	UpdateTrust(context.Context, common.Bundle) error
 	DeleteOrganizationByID(ctx context.Context, orgID uint) error
 	DeleteMemberbyID(ctx context.Context, memberID uint) error
