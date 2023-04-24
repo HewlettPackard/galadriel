b284aadd3398917aec13af9976fb235f3607c0ea
Victor Vieira Barros Leal da Silveira <victorblsilveira@gmail.com>
2023-04-24 15:32:48 -0300

    fix: removing unused functions

--- pkg/common/http/http.go
+++ pkg/common/http/http.go
@@ -1,9 +1,7 @@
 package http
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -12,26 +10,6 @@ import (
 /*
 HandleError default error handler function over the application
 */
-func HandleError(w http.ResponseWriter, errMsg string) error {
-	errBytes := []byte(errMsg)
-	w.WriteHeader(500)
-
-	_, err := w.Write(errBytes)
-	if err != nil {
-		return fmt.Errorf("failed to write error response: %v", err)
-	}
-
-	encoder := json.NewEncoder(w)
-	encoder.SetEscapeHTML(true)
-
-	err = encoder.Encode(errBytes)
-	if err != nil {
-		return fmt.Errorf("failed to write error response: %v", err)
-	}
-
-	return nil
-}
-
 func HandleTCPError(ctx echo.Context, err error) error {
 	_, err = ctx.Response().Write([]byte(err.Error()))
 	if err != nil {
@@ -42,29 +20,8 @@ func HandleTCPError(ctx echo.Context, err error) error {
 }
 
 /*
-WriteAsJSInResponse parses a struct into a json and writes in the response
-w: The response writer to be full filled with the struct response bytes
-out: A pointer to the interface to be writed in the response
+WriteObjectResponse parses a struct into a json and writes in the response
 */
-func WriteAsJSInResponse[T any](w http.ResponseWriter, out *T) error {
-	if out == nil {
-		return nil
-	}
-
-	outBytes, err := json.Marshal(out)
-	if err != nil {
-		return fmt.Errorf("failed marshal response body: %v", err)
-	}
-
-	_, err = w.Write(outBytes)
-	if err != nil {
-		return fmt.Errorf("failed to write response body: %v", err)
-	}
-
-	return nil
-}
-
-// Writes the response to the client
 func WriteObjectResponse[T any](ctx echo.Context, body *T) error {
 	if body == nil {
 		return nil
@@ -73,7 +30,9 @@ func WriteObjectResponse[T any](ctx echo.Context, body *T) error {
 	return write(ctx, body)
 }
 
-// Writes the Array responses to the client
+/*
+WriteArrayResponse parses a slice of object into a json and writes in the response
+*/
 func WriteArrayResponse[T any](ctx echo.Context, body []*T) error {
 	if body == nil {
 		return nil
@@ -90,23 +49,8 @@ func write[T any](ctx echo.Context, body T) error {
 }
 
 /*
-FromJSBody parses json bytes into a struct
-r: Request that contains the json bytes to be parsed into 'in'
-in: Reference(pointer) to the interface to be full filled
+FromBody parses json bytes into a struct
 */
-func FromJSBody[T any](r *http.Request, in *T) (*T, error) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed reading request body: %v", err)
-	}
-
-	if err = json.Unmarshal(body, in); err != nil {
-		return nil, fmt.Errorf("failed unmarshalling request: %v", err)
-	}
-
-	return in, nil
-}
-
 func FromBody[T any](ctx echo.Context, in *T) error {
 	return ctx.Bind(in)
 }
