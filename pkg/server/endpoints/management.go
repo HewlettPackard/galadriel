bfdfcc4a50485905cff642970c250bd40194feb5
Victor Vieira Barros Leal da Silveira <victorblsilveira@gmail.com>
2023-05-05 11:40:37 -0300

    feat: Implementing Admin Handlers

--- pkg/server/endpoints/management.go
+++ pkg/server/endpoints/management.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/HewlettPackard/galadriel/pkg/common/api"
 	"github.com/HewlettPackard/galadriel/pkg/common/entity"
 	chttp "github.com/HewlettPackard/galadriel/pkg/common/http"
 	"github.com/HewlettPackard/galadriel/pkg/server/api/admin"
 	"github.com/HewlettPackard/galadriel/pkg/server/datastore"
+	"github.com/google/uuid"
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
 
 	"github.com/sirupsen/logrus"
 
@@ -89,6 +92,21 @@ func (h AdminAPIHandlers) PutRelationships(ctx echo.Context) error {
 
 // GetRelationshipsRelationshipID retrieve a specific relationship based on its id - (GET /relationships/{relationshipID})
 func (h AdminAPIHandlers) GetRelationshipsRelationshipID(ctx echo.Context, relationshipID api.UUID) error {
+	gctx := ctx.Request().Context()
+
+	r, err := h.Datastore.FindRelationshipByID(gctx, relationshipID)
+	if err != nil {
+		err = fmt.Errorf("failed getting relationships: %v", err)
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
+	response := api.RelationshipFromEntity(r)
+	err = chttp.WriteResponse(ctx, response)
+	if err != nil {
+		err = fmt.Errorf("relationship entity - %v", err.Error())
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
 	return nil
 }
 
@@ -138,16 +156,116 @@ func (h AdminAPIHandlers) PutTrustDomain(ctx echo.Context) error {
 
 // GetTrustDomainTrustDomainName retrieve a specific trust domain by its name - (GET /trust-domain/{trustDomainName})
 func (h AdminAPIHandlers) GetTrustDomainTrustDomainName(ctx echo.Context, trustDomainName api.TrustDomainName) error {
+	gctx := ctx.Request().Context()
+
+	tdName, err := spiffeid.TrustDomainFromString(trustDomainName)
+	if err != nil {
+		err = fmt.Errorf("failed parsing trust domain name: %v", err)
+		return h.HandleAndLog(err, http.StatusBadRequest)
+	}
+
+	td, err := h.Datastore.FindTrustDomainByName(gctx, tdName)
+	if err != nil {
+		err = fmt.Errorf("failed getting trust domain: %v", err)
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
+	response := api.TrustDomainFromEntity(td)
+	err = chttp.WriteResponse(ctx, response)
+	if err != nil {
+		err = fmt.Errorf("trust domain entity - %v", err.Error())
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
 	return nil
 }
 
 // PutTrustDomainTrustDomainName updates the trust domain - (PUT /trust-domain/{trustDomainName})
 func (h AdminAPIHandlers) PutTrustDomainTrustDomainName(ctx echo.Context, trustDomainName api.UUID) error {
+	gctx := ctx.Request().Context()
+
+	reqBody := &admin.PutTrustDomainTrustDomainNameJSONRequestBody{}
+	err := chttp.FromBody(ctx, reqBody)
+	if err != nil {
+		err := fmt.Errorf("failed to read trust domain put body: %v", err)
+		return h.HandleAndLog(err, http.StatusBadRequest)
+	}
+
+	etd, err := reqBody.ToEntity()
+	if err != nil {
+		err := fmt.Errorf("failed to read trust domain put body: %v", err)
+		return h.HandleAndLog(err, http.StatusBadRequest)
+	}
+
+	td, err := h.Datastore.CreateOrUpdateTrustDomain(gctx, etd)
+	if err != nil {
+		err = fmt.Errorf("failed creating/updating trust domain: %v", err)
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
+	h.Logger.Printf("Trust Bundle %v created/updated", td.Name)
+
+	response := api.TrustDomainFromEntity(td)
+	err = chttp.WriteResponse(ctx, response)
+	if err != nil {
+		err = fmt.Errorf("relationships - %v", err.Error())
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
 	return nil
 }
 
 // PostTrustDomainTrustDomainNameJoinToken generate a join token for the trust domain - (POST /trust-domain/{trustDomainName}/join-token)
 func (h AdminAPIHandlers) PostTrustDomainTrustDomainNameJoinToken(ctx echo.Context, trustDomainName api.TrustDomainName) error {
+	gctx := ctx.Request().Context()
+
+	tdName, err := spiffeid.TrustDomainFromString(trustDomainName)
+	if err != nil {
+		err = fmt.Errorf("failed parsing trust domain name: %v", err)
+		return h.HandleAndLog(err, http.StatusBadRequest)
+	}
+
+	td, err := h.Datastore.FindTrustDomainByName(gctx, tdName)
+	if err != nil {
+		err = fmt.Errorf("failed retrieve the trust domain by name %v", err)
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
+	if td == nil {
+		err = fmt.Errorf("trust domain does not exists %v", trustDomainName)
+		return h.HandleAndLog(err, http.StatusBadRequest)
+	}
+
+	token, err := util.GenerateToken()
+	if err != nil {
+		err = fmt.Errorf("failed generating a new join token %v", err)
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
+	jt := &entity.JoinToken{
+		TrustDomainID: td.ID.UUID,
+		Token:         token,
+		ExpiresAt:     time.Now().Add(1 * time.Hour),
+	}
+
+	jt, err = h.Datastore.CreateJoinToken(gctx, jt)
+	if err != nil {
+		err = fmt.Errorf("failed creating the join token: %v", err)
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
+	h.Logger.Printf("join token successfully created for %v", td.Name)
+
+	response := admin.JoinTokenResult{
+		Token: uuid.MustParse(jt.Token),
+	}
+
+	err = chttp.WriteResponse(ctx, response)
+	if err != nil {
+		err = fmt.Errorf("relationships - %v", err.Error())
+		return h.HandleAndLog(err, http.StatusInternalServerError)
+	}
+
 	return nil
 }
 
