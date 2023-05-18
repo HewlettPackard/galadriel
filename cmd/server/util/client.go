5ca38bdffe6cd80f30bf52784bc50803ea1e5abc
Caio Milfont <caiomgb@hotmail.com>
2023-05-18 17:45:48 -0300

    fixing duplication messages

--- cmd/server/util/client.go
+++ cmd/server/util/client.go
@@ -16,8 +16,14 @@ import (
 )
 
 const (
-	jsonContentType = "application/json"
-	baseURL         = "http://localhost/"
+	jsonContentType           = "application/json"
+	baseURL                   = "http://localhost/"
+	errFailedRequest          = "failed to send request: %v"
+	errReadResponseBody       = "failed to read response body: %v"
+	errStatusCodeMsg          = "request returned an error code %d: \n%s"
+	errUnmarshalRelationships = "failed to unmarshal relationships: %v"
+	errUnmarshalTrustDomains  = "failed to unmarshal trust domain: %v"
+	errUnmarshalJoinToken     = "failed to unmarshal join token: %v"
 )
 
 // ServerLocalClient represents a local client of the Galadriel Server.
@@ -57,22 +63,22 @@ type serverClient struct {
 func (c *serverClient) GetTrustDomainByName(ctx context.Context, trustDomainName api.TrustDomainName) (*entity.TrustDomain, error) {
 	res, err := c.client.GetTrustDomainByName(ctx, trustDomainName)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf(errFailedRequest, err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return nil, fmt.Errorf(errReadResponseBody, err)
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request returned an error code %d: \n%s", res.StatusCode, body)
+		return nil, fmt.Errorf(errStatusCodeMsg, res.StatusCode, body)
 	}
 
 	var trustDomain *entity.TrustDomain
 	if err = json.Unmarshal(body, &trustDomain); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal trust domain: %v", err)
+		return nil, fmt.Errorf(errUnmarshalTrustDomains, err)
 	}
 
 	return trustDomain, nil
@@ -82,22 +88,22 @@ func (c *serverClient) UpdateTrustDomainByName(ctx context.Context, trustDomainN
 	payload := api.TrustDomain{Name: trustDomainName}
 	res, err := c.client.PutTrustDomainByName(ctx, trustDomainName, payload)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf(errFailedRequest, err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return nil, fmt.Errorf(errReadResponseBody, err)
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request returned an error code %d: \n%s", res.StatusCode, body)
+		return nil, fmt.Errorf(errStatusCodeMsg, res.StatusCode, body)
 	}
 
 	var trustDomain *entity.TrustDomain
 	if err = json.Unmarshal(body, &trustDomain); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal trust domain: %v", err)
+		return nil, fmt.Errorf(errUnmarshalTrustDomains, err)
 	}
 
 	return trustDomain, nil
@@ -108,22 +114,22 @@ func (c *serverClient) CreateTrustDomain(ctx context.Context, trustDomainName ap
 
 	res, err := c.client.PutTrustDomain(ctx, payload)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf(errFailedRequest, err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return nil, fmt.Errorf(errReadResponseBody, err)
 	}
 
 	if res.StatusCode != http.StatusCreated {
-		return nil, fmt.Errorf("request returned an error code %d: \n%s", res.StatusCode, body)
+		return nil, fmt.Errorf(errStatusCodeMsg, res.StatusCode, body)
 	}
 
 	var trustDomainRes *entity.TrustDomain
 	if err = json.Unmarshal(body, &trustDomainRes); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal trust domain: %v", err)
+		return nil, fmt.Errorf(errUnmarshalTrustDomains, err)
 	}
 
 	return trustDomainRes, nil
@@ -133,7 +139,7 @@ func (c *serverClient) CreateRelationship(ctx context.Context, rel *entity.Relat
 	payload := admin.PutRelationshipJSONRequestBody{TrustDomainAName: rel.TrustDomainAName.String(), TrustDomainBName: rel.TrustDomainBName.String()}
 	res, err := c.client.PutRelationship(ctx, payload)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf(errFailedRequest, err)
 	}
 	defer res.Body.Close()
 
@@ -148,7 +154,7 @@ func (c *serverClient) CreateRelationship(ctx context.Context, rel *entity.Relat
 
 	var relationships *entity.Relationship
 	if err = json.Unmarshal(body, &relationships); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal relationships: %v", err)
+		return nil, fmt.Errorf(errUnmarshalRelationships, err)
 	}
 
 	return relationships, nil
@@ -158,22 +164,22 @@ func (c *serverClient) GetRelationships(ctx context.Context, consentStatus api.C
 
 	res, err := c.client.GetRelationships(ctx, payload)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf(errFailedRequest, err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return nil, fmt.Errorf(errReadResponseBody, err)
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request returned an error code %d: \n%s", res.StatusCode, body)
+		return nil, fmt.Errorf(errStatusCodeMsg, res.StatusCode, body)
 	}
 
 	var relationship *entity.Relationship
 	if err = json.Unmarshal(body, &relationship); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal relationship: %v", err)
+		return nil, fmt.Errorf(errUnmarshalRelationships, err)
 	}
 
 	return relationship, nil
@@ -182,22 +188,22 @@ func (c *serverClient) GetRelationships(ctx context.Context, consentStatus api.C
 func (c *serverClient) GetRelationshipByID(ctx context.Context, relID uuid.UUID) (*entity.Relationship, error) {
 	res, err := c.client.GetRelationshipByID(ctx, relID)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf(errFailedRequest, err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return nil, fmt.Errorf(errReadResponseBody, err)
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request returned an error code %d: \n%s", res.StatusCode, body)
+		return nil, fmt.Errorf(errStatusCodeMsg, res.StatusCode, body)
 	}
 
 	var relationship *entity.Relationship
 	if err = json.Unmarshal(body, &relationship); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal relationship: %v", err)
+		return nil, fmt.Errorf(errUnmarshalRelationships, err)
 	}
 
 	return relationship, nil
@@ -206,22 +212,22 @@ func (c *serverClient) GetRelationshipByID(ctx context.Context, relID uuid.UUID)
 func (c *serverClient) GetJoinToken(ctx context.Context, trustDomainName api.TrustDomainName) (*entity.JoinToken, error) {
 	res, err := c.client.GetJoinToken(ctx, trustDomainName)
 	if err != nil {
-		return nil, fmt.Errorf("failed to send request: %v", err)
+		return nil, fmt.Errorf(errFailedRequest, err)
 	}
 	defer res.Body.Close()
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %v", err)
+		return nil, fmt.Errorf(errReadResponseBody, err)
 	}
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request returned an error code %d: \n%s", res.StatusCode, body)
+		return nil, fmt.Errorf(errStatusCodeMsg, res.StatusCode, body)
 	}
 
 	var joinToken *entity.JoinToken
 	if err = json.Unmarshal(body, &joinToken); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal join token: %v", err)
+		return nil, fmt.Errorf(errUnmarshalJoinToken, err)
 	}
 
 	return joinToken, nil
