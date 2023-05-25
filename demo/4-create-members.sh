dd58fb6720abc4883f0cd007eebe6e65f6ac42a5
Max Lambrecht <maxlambrecht@gmail.com>
2023-05-25 10:41:08 -0500

    Removing unused files
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- demo/4-create-members.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-./bin/galadriel-server create member --trustDomain one.org
-./bin/galadriel-server create member --trustDomain two.org
-./bin/galadriel-server create relationship -a one.org -b two.org
--- demo/x-cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf ./one.org/spire/.data
-rm -rf ./two.org/spire/.data
-rm -rf /tmp/one.org
-rm -rf /tmp/two.org
