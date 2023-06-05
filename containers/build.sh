b02f0f861dabd7b41491f37e942903cb1d665cc0
Victor Vieira Barros Leal da Silveira <victorblsilveira@gmail.com>
2023-06-05 15:31:03 -0300

    feat: removing personal containers

--- containers/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ./database
-docker build -t galadrieldb .
-cd -
-
-cd ./pgadmin
-docker build -t pgadmin .
-cd -
--- containers/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d --name galadrieldb -p 5432:5432 galadrieldb
-docker run -d --name pgadmin -p 82:80 pgadmin
-
