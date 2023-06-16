8b4982f70bb265f4aad0571eede3a543ee6e7f7d
Max Lambrecht <maxlambrecht@gmail.com>
2023-06-16 11:46:50 -0500

    Add tests
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

---
+++ pkg/server/endpoints/helper_test.go
@@ -0,0 +1,70 @@
+package endpoints
+
+import (
+	"testing"
+
+	"github.com/HewlettPackard/galadriel/pkg/common/api"
+	"github.com/HewlettPackard/galadriel/pkg/common/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockParams struct {
+	pageSize      *int
+	pageNumber    *int
+	consentStatus *api.ConsentStatus
+}
+
+func (m *mockParams) GetPageSize() *int {
+	return m.pageSize
+}
+
+func (m *mockParams) GetPageNumber() *int {
+	return m.pageNumber
+}
+
+func (m *mockParams) GetConsentStatus() *api.ConsentStatus {
+	return m.consentStatus
+}
+
+func TestConvertRelationshipsParamsToListCriteria(t *testing.T) {
+	ps := 1
+	pn := 1
+	cs := api.Approved
+
+	params := &mockParams{
+		pageSize:      &ps,
+		pageNumber:    &pn,
+		consentStatus: &cs,
+	}
+
+	result, err := convertRelationshipsParamsToListCriteria(params)
+	assert.Nil(t, err)
+
+	assert.Equal(t, result.PageSize, uint(ps))
+	assert.Equal(t, result.PageNumber, uint(pn))
+	assert.Equal(t, *result.FilterByConsentStatus, entity.ConsentStatus(cs))
+}
+
+func TestConvertPaginationParam(t *testing.T) {
+	value := 1
+
+	result, err := convertPaginationParam(&value)
+	assert.Nil(t, err)
+	assert.Equal(t, result, uint(value))
+
+	value = -1
+	result, err = convertPaginationParam(&value)
+	assert.NotNil(t, err)
+}
+
+func TestConvertValidConsentStatusParam(t *testing.T) {
+	cs := api.Approved
+
+	result, err := convertValidConsentStatusParam(&cs)
+	assert.Nil(t, err)
+	assert.Equal(t, *result, entity.ConsentStatus(cs))
+
+	cs = "invalid"
+	result, err = convertValidConsentStatusParam(&cs)
+	assert.NotNil(t, err)
+}
